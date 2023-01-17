9d58d5c2fa5c8ccaea49365f611b55a00afb89eb
Vlad Zusko <vladzvu11@gmail.com>
2023-01-17 21:36:53 +0200

    feat: add layout setting in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { ReactNode } from 'react';
+import type { AppProps as NextAppProps } from 'next/app';
+
+import type { Page } from '@/core/types';
+import { Layout } from '@/components/layouts';
+import Head from 'next/head';
+
+interface AppProps extends NextAppProps {
+  Component: Page;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const getLayout =
+    Component.getLayout ?? ((page: ReactNode) => <Layout>{page}</Layout>);
+
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
 }
