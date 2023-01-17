41ca05792e07b95b6fe51bdc0a08490504abd5e8
Vlad Zusko <vladzvu11@gmail.com>
2023-01-17 23:53:04 +0200

    feat(store): add action naming util

---
+++ src/store/utils/getActionNameCreator.ts
@@ -0,0 +1,4 @@
+const getActionNameCreator = (slice: string) => (name: string) =>
+  `${slice}/${name}`;
+
+export default getActionNameCreator;
---
+++ src/store/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as getActionNameCreator } from './getActionNameCreator';
