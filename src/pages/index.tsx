d520a0c3501c08c58763d671a58454ea32663b92
Vlad Zusko <vladzvu11@gmail.com>
2023-01-17 22:07:30 +0200

    feat(pages): add home title setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { useTranslation } from '#/localization/hooks';
 
+import wrapper from '@/store';
+import { getT } from '#/localization/utils';
+
 export default function Home() {
   const { t } = useTranslation();
 
   return <>{t('common:company.name')}</>;
 }
+
+export const getStaticProps = wrapper.getStaticProps(
+  () =>
+    async ({ locale }) => {
+      const t = await getT(locale, ['pages/home']);
+      const title = t('pages/home:meta.title');
+
+      return {
+        props: {
+          title,
+        },
+      };
+    },
+);
