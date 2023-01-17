6469f30617cb3e1aead5eeabaa78fec173319a61
Vlad Zusko <vladzvu11@gmail.com>
2023-01-17 23:57:28 +0200

    feat(store): connect ui reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
 
-export const createStore = () => configureStore({ reducer: {} });
+import uiReducer from '#/ui/slice';
+
+export const createStore = () =>
+  configureStore({
+    reducer: {
+      ui: uiReducer,
+    },
+  });
 
 const wrapper = createWrapper(createStore);
 
