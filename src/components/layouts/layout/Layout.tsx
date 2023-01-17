f276d8a1988603dbe6db01458ed61f95fdc6cd83
Vlad Zusko <vladzvu11@gmail.com>
2023-01-18 00:31:19 +0200

    feat(component): improve default layout

--- src/components/layouts/layout/Layout.tsx
+++ src/components/layouts/layout/Layout.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from 'react';
 
+import { Box } from '@/components/common';
+
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  return <>{children}</>;
+  return <Box>{children}</Box>;
 }
