329d2d2fe079b9c008f0541d1e106386279fdef9
Vlad Zusko <vladzvu11@gmail.com>
2023-01-17 22:06:55 +0200

    feat(pages): connect store to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,32 @@
 import type { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import Head from 'next/head';
 import type { AppProps as NextAppProps } from 'next/app';
 
 import type { Page } from '@/core/types';
+import { DEFAULT_TITLE } from '@/core/constants';
 import { Layout } from '@/components/layouts';
-import Head from 'next/head';
+import wrapper from '@/store';
+import { useTranslation } from '#/localization/hooks';
 
 interface AppProps extends NextAppProps {
   Component: Page;
 }
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, ...props }: AppProps) {
+  const { store, props: updatedProps } = wrapper.useWrappedStore(props);
+  const { t } = useTranslation();
+
   const getLayout =
     Component.getLayout ?? ((page: ReactNode) => <Layout>{page}</Layout>);
 
   return (
-    <>
+    <Provider store={store}>
       <Head>
-        <title></title>
+        <title>{updatedProps.pageProps.title ?? t(DEFAULT_TITLE)}</title>
       </Head>
-      {getLayout(<Component {...pageProps} />)}
-    </>
+
+      {getLayout(<Component {...updatedProps.pageProps} />)}
+    </Provider>
   );
 }
