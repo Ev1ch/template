7414b2e4e5bbffe4690906ae8a27e0436cf50b7b
Vlad Zusko <vladzvu11@gmail.com>
2023-01-17 22:05:48 +0200

    feat(store): add thunk creator

---
+++ src/store/creators/createAsyncThunk.ts
@@ -0,0 +1,24 @@
+import {
+  createAsyncThunk as createReduxAsyncThunk,
+  type AsyncThunkPayloadCreator,
+  type AsyncThunkOptions,
+} from '@reduxjs/toolkit';
+
+import { serialize } from '#/errors/utils';
+import type { ThunkOptions } from '../types';
+
+const createAsyncThunk = <Returned = void, Argument = void>(
+  typePrefix: string,
+  payloadCreator: AsyncThunkPayloadCreator<Returned, Argument, ThunkOptions>,
+  options?: AsyncThunkOptions<Argument, ThunkOptions>,
+) =>
+  createReduxAsyncThunk<Returned, Argument, ThunkOptions>(
+    typePrefix,
+    payloadCreator,
+    {
+      serializeError: serialize,
+      ...options,
+    },
+  );
+
+export default createAsyncThunk;
