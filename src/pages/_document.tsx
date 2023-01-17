6be7a89fa30245795b8b51e862b9fa9835ef531d
Vlad Zusko <vladzvu11@gmail.com>
2023-01-17 22:07:13 +0200

    feat(pages): add auto lang attribute setting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  type DocumentProps,
+} from 'next/document';
 
-export default function Document() {
+export default function Document({ locale }: DocumentProps) {
   return (
-    <Html lang="en">
+    <Html lang={locale}>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
