f6914463563e74845ae5307c90a7bcc6fd19273e
Vlad Zusko <vladzvu11@gmail.com>
2023-01-17 23:33:05 +0200

    feat(styles): update font options

--- src/styles/fonts/index.ts
+++ src/styles/fonts/index.ts
@@ -3,4 +3,6 @@ import { Roboto } from '@next/font/google';
 export const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
   subsets: ['cyrillic-ext', 'latin-ext'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
 });
