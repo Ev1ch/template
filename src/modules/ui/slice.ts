2c43a001e2225791a014b60de85cdc32cee7dfc5
Vlad Zusko <vladzvu11@gmail.com>
2023-01-18 17:28:06 +0200

    feat(ui): add pending actions tracking

--- src/modules/ui/slice.ts
+++ src/modules/ui/slice.ts
@@ -1,17 +1,30 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+  createSlice,
+  isFulfilled,
+  isPending,
+  isRejected,
+  type PayloadAction,
+} from '@reduxjs/toolkit';
 
 import type { State } from '@/store/types';
+import { getActionRootType } from '@/store/utils';
 import { ThemeMode } from '@/styles/types';
 
 interface UiState {
   theme: {
     mode: ThemeMode;
   };
+  statuses: {
+    pending: Record<string, boolean>;
+  };
 }
 
 const initialState: UiState = {
   theme: {
     mode: ThemeMode.DARK,
+  },
+  statuses: {
+    pending: {},
   },
 };
 
@@ -24,6 +37,20 @@ const slice = createSlice({
     setThemeMode: (state, { payload }: PayloadAction<ThemeMode>) => {
       state.theme.mode = payload;
     },
+  },
+  extraReducers(builder) {
+    builder.addMatcher(isPending, (state, action) => {
+      const type = getActionRootType(action);
+      state.statuses.pending[type] = true;
+    });
+    builder.addMatcher(isFulfilled, (state, action) => {
+      const type = getActionRootType(action);
+      delete state.statuses.pending[type];
+    });
+    builder.addMatcher(isRejected, (state, action) => {
+      const type = getActionRootType(action);
+      delete state.statuses.pending[type];
+    });
   },
 });
 
